09348f3e6c4e6df63240dc2ceb6bd1f0469f863e
NateBowers <natebowersnc@gmail.com>
2025-01-24 15:57:08 -0500

    Removed tabs

--- Data_Cleanup.py
+++ Data_Cleanup.py
@@ -1,17 +1,31 @@
 import pandas as pd
-
-
-df = pd.read_csv("2025_Problem_C_Data/summerOly_athletes.csv")
-
-countries = df[["Team", "NOC"]].copy().drop_duplicates().sort_values(by=['NOC'], ignore_index=True)
-print(countries)
-print(countries.dtypes)
+import numpy as np
+import csv
 
 
 
 
-# countries.to_csv('countries_list', index=False, header=False)
+
+countriesDict = pd.read_csv("countries.csv")
+print(countriesDict)
 
 
+
+
+
+
+
+# df = pd.read_csv("2025_Problem_C_Data/summerOly_athletes.csv")
+
+# countries = df[["Team", "NOC"]].copy().drop_duplicates().sort_values(by=['NOC'], ignore_index=True)
+# print(countries)
+# print(countries.dtypes)
+
+
+
+
+# # countries.to_csv('countries_list', index=False, header=False)
+
+# np.set_printoptions(threshold=np.inf)
 # array = countries.to_numpy()
 # print(array)
