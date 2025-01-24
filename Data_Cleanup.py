e53535c7c19e3ca590cad5360b207ba00167523d
NateBowers <natebowersnc@gmail.com>
2025-01-24 16:09:02 -0500

    code

--- Data_Cleanup.py
+++ Data_Cleanup.py
@@ -3,29 +3,8 @@ import numpy as np
 import csv
 
 
+athletes = pd.read_csv("2025_Problem_C_Data/summerOly_athletes.csv")
+country_codes = pd.read_csv("countries.csv", index_col=False)
 
+NOC_dict = dict(zip(country_codes["NOC"], country_codes["Country"]))
 
-
-countriesDict = pd.read_csv("countries.csv")
-print(countriesDict)
-
-
-
-
-
-
-
-# df = pd.read_csv("2025_Problem_C_Data/summerOly_athletes.csv")
-
-# countries = df[["Team", "NOC"]].copy().drop_duplicates().sort_values(by=['NOC'], ignore_index=True)
-# print(countries)
-# print(countries.dtypes)
-
-
-
-
-# # countries.to_csv('countries_list', index=False, header=False)
-
-# np.set_printoptions(threshold=np.inf)
-# array = countries.to_numpy()
-# print(array)
