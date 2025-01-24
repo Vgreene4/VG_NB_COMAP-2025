ee591ed26c7150a38bb80e1060c169599ea8e50c
NateBowers <natebowersnc@gmail.com>
2025-01-24 18:14:05 -0500

    Removed explicit indexes from csv

--- histograms.py
+++ histograms.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-countries = pd.read_csv("countries.csv", dtype=str)[1]
+
+countries = pd.read_csv("Clean Data/countries.csv", dtype=str, delimiter=",")
+medals = pd.read_csv("Clean Data/summerOly_medals_counts_clean.csv", delimiter=",", dtype=str, index_col=False)
+
 
 print(countries)
+print(medals)
