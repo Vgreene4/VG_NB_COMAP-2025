1e20434c458d5b3d574e9ffb625c2ede7b6815ec
NateBowers <natebowersnc@gmail.com>
2025-01-26 19:30:06 -0500

    more progress on formatting data for training

--- data_formatter.py
+++ data_formatter.py
@@ -37,38 +37,66 @@ yi = [
     # total
 ]
 
-A data point only exists IF a team competed that year
-
-
+A data point only exists IF a team competed that year, otherwise there is none.
 
 '''
 
 # data that is just year dependent: num athletes, num countries, num events, num sports, host
 # data that is year and country dependent: num athletes, 1st year, num olympics competed
 
-
-
-
 # first, make a list of all NOC, year pairs
 
 
 athlete_df = pd.read_csv('Clean Data/summerOly_athletes_cropped.csv', usecols=[0,3,4])
 athlete_list = athlete_df.to_numpy(dtype=str)
-print(athlete_list)
+# print(athlete_list)
 
 country_list = np.loadtxt('Clean Data/summerOly_countries_cropped.csv', delimiter=",", dtype=str)
-medal_counts = np.loadtxt('Clean Data/summerOly_medals_clean.csv', delimiter=",", dtype=str, skiprows=1, usecols=[3,4,5,7,8])
+medal_counts = np.loadtxt('Clean Data/summerOly_medals_clean.csv', delimiter=",", dtype=str, skiprows=1, usecols=[3,4,5,6,7,8])
 # athlete_list = np.loadtxt('Clean Data/summerOly_athletes_cropped.csv', delimiter=",", skiprows=1, usecols=[0,3,4], dtype=[('Name', str), ('NOC', str), ('Year', int)])
 # host_list = np.loadtxt('2025_Problem_C_Data/summerOly_hosts.csv', delimiter=',', dtype=str)
 
-print(athlete_df)
+programs_list = np.loadtxt('Clean Data/summerOly_programs_cropped.csv', delimiter=',', dtype=str)
+
+print(programs_list.T)
+
+# print(athlete_df)
 
 # print(medal_counts)
 
 competed_arr, num_athletes = np.unique(athlete_list[:,1:], axis=0, return_counts=True)
 
-print(competed_arr)
-print(competed_arr.shape)
+# print(competed_arr)
+
+for i,row in enumerate(competed_arr):
+    year, country = row[1], row[0]
+    # print(num_athletes[i])
+    print('country/year')
+    print(row)
+    # print(year)
+    for e in programs_list.T:
+        if e[0] == year:
+            print("events/disciplines/sports")
+            print(e[1:])
+
+    score = [0,0,0,0]
+    for e in medal_counts:
+        if e[4] == year and e[5] == country:
+            score = e[0:3]
+    print('medals')
+    print(score)
+    print('\n')
+
+# for row in competed_arr:
+    # print(row)
+
+
+
+
+
+
+# print(competed_arr)
+# print(competed_arr.shape)
 
 # for i in competed_arr:
 
